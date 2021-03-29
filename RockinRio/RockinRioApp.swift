d9a579a6d8ff8d5ee68d56c2ea44b3b31eddd95a
Aline Escobar <aline.escobar@outlook.com>
2021-03-29 14:56:13 -0300

    Initial Commit

---
+++ RockinRio/RockinRioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockinRioApp.swift
+//  RockinRio
+//
+//  Created by Aline Osana Escobar on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct RockinRioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
