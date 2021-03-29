831b9d1c5f3e8ea0a73f4f7c8f28d37763ce05e8
Aline Escobar <aline.escobar@outlook.com>
2021-03-29 16:13:38 -0300

    estrutura de dados

---
+++ RockinRio/Show.swift
@@ -0,0 +1,33 @@
+//
+//  Show.swift
+//  RockinRio
+//
+//  Created by Aline Osana Escobar on 29/03/21.
+//
+
+import Foundation
+
+//formato de dado para apresentaçoes
+[
+{
+"nomeDaBanda": "Nome da banda",
+"dataDoShow":"01/10/21",
+"horario":"22:00",
+"palco":"mundo",
+"favorito": false,
+"imagem": "nome da imagem",
+"descricao": "descricao"
+},
+]
+
+//formato de dado para perfil
+[
+{
+"nome":"Nome da pessoa",
+"dataNascimento":"09/11/1994",
+"imagemPerfil": "nome da imagem",
+"email":"email@email.com",
+"senha": "12345",
+"showFavoritado": [Show01, Show02]
+}
+]
